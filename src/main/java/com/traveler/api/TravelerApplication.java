83bb3d3ca4beca5ddfb026f1a0361e1d5ca46ec5
Gabriel <gabriel@webcgi.com.br>
2024-09-16 15:00:43 -0300

    :sparkles: feat: inicia criação do projeto e conexão do banco local

---
+++ src/main/java/com/traveler/api/TravelerApplication.java
@@ -0,0 +1,13 @@
+package com.traveler.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traveler/api/TravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traveler.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
