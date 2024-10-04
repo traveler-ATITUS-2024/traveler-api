fabbbb6dc5be77c9f63b6ca1efee35cf7f166b5b
Gabriel <gabriel@webcgi.com.br>
2024-10-04 08:23:52 -0300

    :sparkles: feat: Adiciona redirecionamento de rota

---
+++ src/main/java/com/traveler/api/controller/RedirectController.java
@@ -0,0 +1,13 @@
+package com.traveler.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
+    }
+}
