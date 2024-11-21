86c5fa6c60fe71b15aced6d30237986cec1f4914
Gabriel <gabriel@webcgi.com.br>
2024-11-21 14:52:01 -0300

    :sparkles: feat: Requisição de despesas vinculadas as categorias

--- src/main/java/com/traveler/api/repository/DespesaRepository.java
+++ src/main/java/com/traveler/api/repository/DespesaRepository.java
@@ -2,10 +2,12 @@ package com.traveler.api.repository;
 
 import com.traveler.api.entity.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface DespesaRepository extends JpaRepository<Despesa, Long> {
     List<Despesa> findByViagemId(Long viagemId);
+
 }
 
--- src/main/java/com/traveler/api/repository/ViagemRepository.java
+++ src/main/java/com/traveler/api/repository/ViagemRepository.java
@@ -1,11 +1,25 @@
 package com.traveler.api.repository;
 
+import com.traveler.api.controller.dto.CategoriaDespesaDto;
+import com.traveler.api.entity.Despesa;
 import com.traveler.api.entity.Viagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
+@Repository
 public interface ViagemRepository extends JpaRepository<Viagem, Long> {
 
     List<Viagem> findByUsuarioId(Long usuarioId);
+
+    @Query("SELECT new com.traveler.api.controller.dto.CategoriaDespesaDto(d.categoriaId, c.nome, SUM(d.valor)) " +
+            "FROM Despesa d " +
+            "JOIN Categoria c ON d.categoriaId = c.id " +
+            "WHERE d.viagemId = :viagemId " +
+            "GROUP BY d.categoriaId, c.nome")
+    List<CategoriaDespesaDto> buscarTotalDespesasPorCategorias(@Param("viagemId") Long viagemId);
 }
